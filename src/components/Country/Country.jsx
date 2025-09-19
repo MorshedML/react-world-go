3487e415d34a63afa267ce8313e2614686ba3ef3
MorshedML <mdmorshed686374@gmail.com>
2025-09-20 04:05:30 +0600

    create different folder and create a new folder for county component

---
+++ src/components/Country/Country.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Country = () => {
+    return (
+        <div>
+            <h1>I am come from Country component</h1>
+        </div>
+    );
+};
+
+export default Country;
