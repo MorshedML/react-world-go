71685632dbe877204bc836f2dfa99383b8c8c4c0
MorshedML <mdmorshed686374@gmail.com>
2025-09-20 03:56:10 +0600

    Project setup

---
+++ src/components/Countries.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h1>I am come from Countries component</h1>
+        </div>
+    );
+};
+
+export default Countries;
