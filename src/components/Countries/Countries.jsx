f68126c17c22ac17797f4501b9bbcfee85968a1f
MorshedML <mdmorshed686374@gmail.com>
2025-09-20 23:23:43 +0600

    we add css for design county and countrise

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,15 +1,24 @@
 import React, { use } from 'react';
+import Country from '../Country/Country';
+import './Countries.css'
 
 const Countries = ({countriesPromise}) => {
     const countriesPromiseData = use(countriesPromise);
     const countrise = countriesPromiseData.countries;
-    console.log(countrise);
     
     
     
     return (
         <div>
             <h1>I am come from Countries component Data is {countrise.length} </h1>
+
+
+            <div className='countries'>
+                {
+                countrise.map(country => <Country key={country.cca3.cca3} country={country}></Country> )
+                }
+            </div>
+
         </div>
     );
 };
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import './Country.css'
 
-const Country = () => {
+const Country = ({country}) => {
+    // console.log(country);
+
+    const handleVisitCountry = () =>{
+        console.log(`btn clicked`);
+    }
+    
     return (
-        <div>
-            <h1>I am come from Country component</h1>
+        <div className='country'>
+            <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
+            <h3>Name: {country.name.common}</h3>
+            <p>Population: {country.population.population}</p>
+            <p>Area: {country.area.area} {country.area.area > 300000 ? 'Big Country' : 'Small Country'}  </p>
+            <button onClick={handleVisitCountry}>Not Visited</button>
         </div>
     );
 };
